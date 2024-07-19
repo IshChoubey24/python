d2a18e06c64075b7e0cb8843e8f26f17cc4b8816
IshCHoubey24 <you@example.com>
2024-07-19 19:17:19 +0530

    strings1

---
+++ String1.py
@@ -0,0 +1,20 @@
+name="ISH "
+
+last_name= name[1:]
+
+print('Fi' + last_name)
+
+x= "Hello"
+y= "Seasme Streets Sundays"
+
+#concat
+print(x + " "+ name + " "+ y)
+
+a=x.upper()
+print(a) 
+
+b=name.lower()
+print(b)
+
+e=y.split('s')
+print(e)
